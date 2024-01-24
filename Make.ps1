81e8543efe8caa4f70a08264162efc3d4b615b0b
rinrab <tima@chemodax.net>
2024-01-24 18:28:01 +0100

    Factor-out NewCommand to function and transform all commands to `Invoke-Svn{action}`

--- Make.ps1
+++ Make.ps1
@@ -1,12 +1,6 @@
 # TODO: Correct filename
 $exeFileName = "C:\Program Files\TortoiseSVN\bin\svn.exe"
 
-$parameters = @{
-    Verb         = 'Get'
-    Noun         = 'SvnStatus'
-    OriginalName = $exeFileName
-}
-
 function FormatParameter {
     param (
         [string]$ParameterName
@@ -14,67 +8,80 @@ function FormatParameter {
     $ParameterName.Replace("-", "").Replace("verbose", "full")
 }
 
-$newCommand = New-CrescendoCommand @parameters
-$newCommand.OriginalCommandElements = @('status')
+function NewCommand {
+    param (
+        [string]$CommandName
+    )
+    
+    $parameters = @{
+        Verb         = 'Invoke'
+        Noun         = 'Svn' + (Get-Culture).TextInfo.ToTitleCase($CommandName)
+        OriginalName = $exeFileName
+    }
 
-$out = svn.exe help status
-
-$i = 0;
-while ($i -lt $out.Length -and $out[$i] -ne "Valid options:") {
+    $newCommand = New-CrescendoCommand @parameters
+    $newCommand.OriginalCommandElements = @($CommandName)
+    
+    $out = svn.exe help $CommandName
+    
+    $i = 0;
+    while ($i -lt $out.Length -and $out[$i] -ne "Valid options:") {
+        $i++
+    }
     $i++
-}
-$i++
-$parameters = @()
-while ($i -lt $out.Length) {
-    $line = $out[$i]
-    if ($line -match " : ") {
-        $parameterNames = @()
+    $parameters = @()
+    while ($i -lt $out.Length) {
+        $line = $out[$i]
+        if ($line -match " : ") {
+            $parameterNames = @()
 
-        if ($line -match " -([a-z]([a-z]|-)*)") {
-            $parameterNames += FormatParameter $Matches[1]
+            if ($line -match " -([a-z]([a-z]|-)*)") {
+                $parameterNames += FormatParameter $Matches[1]
+            }
+            if ($line -match "--([a-z]([a-z]|-)*)") {
+                $parameterNames += FormatParameter $Matches[1]
+            }
+
+            $null = $line -match ": (.*)"
+            $description = $Matches[1]
+
+            $isParameter = $line -match "ARG"
+
+            $parameters += @{
+                ParameterNames = $parameterNames
+                Description    = $description
+                Type           = $isParameter ? "string" : "switch"
+            }
         }
-        if ($line -match "--([a-z]([a-z]|-)*)") {
-            $parameterNames += FormatParameter $Matches[1]
+        else {
+            $null = $line -match " {29}(.*)"
+            $description = $Matches[1]
+            $parameters[-1].Description += "`n$description"
         }
 
-        $null = $line -match ": (.*)"
-        $description = $Matches[1]
-
-        $isParameter = $line -match "ARG"
-
-        $parameters += @{
-            ParameterNames    = $parameterNames
-            Description       = $description
-            Type              = $isParameter ? "string" : "switch"
-        }
-    }
-    else {
-        $null = $line -match " {29}(.*)"
-        $description = $Matches[1]
-        $parameters[-1].Description += "`n$description"
-    }
-
-    $i++
-}
-
-$parameters | Format-Table
-
-foreach ($parameter in $parameters) {
-    $originalName = ($parameter.ParameterNames[0].Length -eq 1) ? "-" + $parameter.ParameterNames[0] : "--" + $parameter.ParameterNames[0]
-    $newParameter = New-ParameterInfo -OriginalName $originalName -Name $parameter.ParameterNames[-1]
-
-    $newParameter.ParameterType = $parameter.Type
-    $newParameter.NoGap = $false
-    $newParameter.Description = $parameter.Description
-
-    for ($i = 0; $i -lt $parameter.ParameterNames.Count - 1; $i++) {
-        $newParameter.Aliases += $parameter.ParameterNames[$i]
+        $i++
     }
 
-    $newCommand.Parameters += $newParameter
+    foreach ($parameter in $parameters) {
+        $originalName = ($parameter.ParameterNames[0].Length -eq 1) ? "-" + $parameter.ParameterNames[0] : "--" + $parameter.ParameterNames[0]
+        $newParameter = New-ParameterInfo -OriginalName $originalName -Name $parameter.ParameterNames[-1]
+
+        $newParameter.ParameterType = $parameter.Type
+        $newParameter.NoGap = $false
+        $newParameter.Description = $parameter.Description
+
+        for ($i = 0; $i -lt $parameter.ParameterNames.Count - 1; $i++) {
+            $newParameter.Aliases += $parameter.ParameterNames[$i]
+        }
+
+        $newCommand.Parameters += $newParameter
+    }
+
+    $newCommand
 }
 
-$CrescendoCommands = @($newCommand)
+$CrescendoCommands = @()
+$CrescendoCommands += NewCommand "status"
 
 if (!(Test-Path .\out\)) {
     $null = mkdir .\out\
