54d17b5dd586372fcc4417e5aed044ddcb009aff
rinrab <tima@chemodax.net>
2024-01-24 14:44:13 +0100

    Add some parameters to command.

--- Make.ps1
+++ Make.ps1
@@ -10,6 +10,18 @@ $parameters = @{
 $newCommand = New-CrescendoCommand @parameters
 $newCommand.OriginalCommandElements = @('status')
 
+$parameter = New-ParameterInfo -OriginalName '--show-updates' -Name 'ShowUpdates'
+$parameter.ParameterType = 'switch'
+$parameter.NoGap = $true
+$parameter.Description = "display update information"
+$newCommand.Parameters += $parameter
+
+$parameter = New-ParameterInfo -OriginalName '-v' -Name 'v'
+$parameter.ParameterType = 'switch'
+$parameter.NoGap = $true
+$parameter.Description = "print extra information"
+$newCommand.Parameters += $parameter
+
 $CrescendoCommands += $newCommand
 
 if (!(Test-Path .\out\)) {
