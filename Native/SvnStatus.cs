f65c383f3b1330445c5e0b8f095466ffc3c0d94f
rinrab <tima@chemodax.net>
2024-01-29 15:32:14 +0100

    Import SvnPosh 'Native'

---
+++ Native/SvnStatus.cs
@@ -0,0 +1,43 @@
+﻿using SharpSvn;
+using System;
+using System.Management.Automation;
+
+namespace SvnPosh
+{
+    [Cmdlet("Invoke", "SvnStatus")]
+    [OutputType(typeof(SvnStatusOutput))]
+    public class SvnStatus : Cmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public string Path { get; set; }
+
+        [Parameter()]
+        public SwitchParameter All { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            using (SvnClient client = new SvnClient())
+            {
+                client.Status(Path, new SvnStatusArgs()
+                    {
+                        RetrieveAllEntries = All
+                    },
+                    new EventHandler<SvnStatusEventArgs>((sender, e) =>
+                    {
+                        WriteObject(new SvnStatusOutput
+                        {
+                            Status = e.LocalNodeStatus,
+                            Path = e.Path
+                        });
+                    }));
+            }
+        }
+    }
+
+    public class SvnStatusOutput
+    {
+        public SharpSvn.SvnStatus Status { get; set; }
+
+        public string Path { get; set; }
+    }
+}
