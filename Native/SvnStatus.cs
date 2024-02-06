d6eae2ae784322d1db70d19030a9cd9981daaec6
tima <tima@chemodax.net>
2024-02-06 06:52:22 +0300

    Use working directory for path

--- Native/SvnStatus.cs
+++ Native/SvnStatus.cs
@@ -1,5 +1,7 @@
 ﻿using SharpSvn;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Management.Automation;
 
 namespace SvnPosh
@@ -7,31 +9,77 @@ namespace SvnPosh
     [Cmdlet("Invoke", "SvnStatus")]
     [Alias("svn-status")]
     [OutputType(typeof(SvnStatusOutput))]
-    public class SvnStatus : Cmdlet
+    public class SvnStatus : PSCmdlet
     {
-        [Parameter(Mandatory = true)]
-        public string Path { get; set; }
-
         [Parameter()]
         public SwitchParameter All { get; set; }
+
+        [Parameter(ValueFromRemainingArguments = true)]
+        public string[] Path { get; set; } = new string[] { "" };
 
         protected override void ProcessRecord()
         {
             using (SvnClient client = new SvnClient())
             {
-                client.Status(Path, new SvnStatusArgs()
+                string[] resolvedPaths = GetPathTargets(Path, null);
+
+                foreach (string resolvedPath in resolvedPaths)
+                {
+                    try
                     {
-                        RetrieveAllEntries = All
-                    },
-                    new EventHandler<SvnStatusEventArgs>((sender, e) =>
+                        client.Status(
+                            resolvedPath,
+                            new SvnStatusArgs()
+                            {
+                                RetrieveAllEntries = All
+                            },
+                            new EventHandler<SvnStatusEventArgs>((sender, e) =>
+                                {
+                                    WriteObject(new SvnStatusOutput
+                                    {
+                                        Status = e.LocalNodeStatus,
+                                        Path = e.Path
+                                    });
+                                }));
+                    }
+                    catch (SvnException ex)
                     {
-                        WriteObject(new SvnStatusOutput
+                        if (ex.ContainsError(SvnErrorCode.SVN_ERR_WC_NOT_WORKING_COPY,
+                                             SvnErrorCode.SVN_ERR_WC_PATH_NOT_FOUND))
                         {
-                            Status = e.LocalNodeStatus,
-                            Path = e.Path
-                        });
-                    }));
+                            WriteWarning(ex.Message);
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                }
             }
+        }
+
+        protected string[] GetPathTargets(string[] pathList, string[] literalPathList)
+        {
+            List<string> result = new List<string>();
+            if (literalPathList != null)
+            {
+                foreach (var literalPath in literalPathList)
+                {
+                    var unresolvedPath = GetUnresolvedProviderPathFromPSPath(literalPath);
+                    result.Add(unresolvedPath);
+                }
+            }
+            else if (pathList != null)
+            {
+                ProviderInfo provider;
+                foreach (var path in pathList)
+                {
+                    var resolvedPath = GetResolvedProviderPathFromPSPath(path, out provider);
+                    result.AddRange(resolvedPath);
+                }
+            }
+
+            return result.ToArray();
         }
     }
 
