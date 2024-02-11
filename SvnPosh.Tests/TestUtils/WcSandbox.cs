cd7d8106dbaeedd672f52cbce64ccb99e4628a06
rinrab <tima@chemodax.net>
2024-02-11 14:22:00 +0100

    Use svnadmin-create in tests

--- SvnPosh.Tests/TestUtils/WcSandbox.cs
+++ SvnPosh.Tests/TestUtils/WcSandbox.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Management.Automation;
+using System.Reflection;
 using SharpSvn;
 
 namespace SvnPosh.Tests.TestUtils
@@ -11,17 +14,25 @@ namespace SvnPosh.Tests.TestUtils
 
         public WcSandbox()
         {
+            PowerShell ps = PowerShell.Create();
+
+            var assemblyDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            ps.AddCommand("ipmo").AddArgument(Path.Combine(assemblyDirectory, @"..\SvnPosh\SvnPosh.psd1")).Invoke();
+
             var reposPath = Path.Combine(RootPath, "repos");
-            using (var client = new SvnRepositoryClient())
-            {
-                client.CreateRepository(reposPath);
-            }
+            Directory.CreateDirectory(reposPath);
+            ps.AddScript($"svnadmin-create '{reposPath}'").Invoke();
 
             ReposUrl = "file:///" + reposPath.Replace('\\', '/');
             var wcPath = Path.Combine(RootPath, "wc");
             using (var client = new SvnClient())
             {
                 client.CheckOut(new SvnUriTarget(ReposUrl), wcPath);
+            }
+
+            foreach (var err in ps.Streams.Error)
+            {
+                Console.Error.WriteLine(err);
             }
 
             ReposPath = reposPath;
