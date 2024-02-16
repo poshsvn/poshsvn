9257eb18f8621ede6cf57e4e076b8002b015038e
rinrab <tima@chemodax.net>
2024-02-16 17:49:27 +0100

    Fix path formatting

---
+++ PoshSvn/PathUtils.cs
@@ -0,0 +1,21 @@
+﻿namespace PoshSvn
+{
+    public class PathUtils
+    {
+        public static string GetRelativePath(string root, string path)
+        {
+            if (path.StartsWith(root + "\\"))
+            {
+                return path.Substring(root.Length + 1);
+            }
+            else if (root == path)
+            {
+                return ".";
+            }
+            else
+            {
+                return path;
+            }
+        }
+    }
+}
