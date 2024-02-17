88120a43949074211de07d86eef876ca5dce856d
rinrab <tima@chemodax.net>
2024-02-17 11:50:40 +0100

    Remove comment

--- PoshSvn.Tests/PathUtilsTests.cs
+++ PoshSvn.Tests/PathUtilsTests.cs
@@ -12,7 +12,7 @@ namespace PoshSvn.Tests
             ClassicAssert.AreEqual(@"C:\b", PathUtils.GetRelativePath(@"C:\a", @"C:\b"));
             ClassicAssert.AreEqual(@"C:\ab", PathUtils.GetRelativePath(@"C:\a", @"C:\ab"));
             ClassicAssert.AreEqual(@"C:\a\b", PathUtils.GetRelativePath(@"C:\a\b\c", @"C:\a\b"));
-            ClassicAssert.AreEqual(@"b\c", PathUtils.GetRelativePath(@"C:\a", @"C:\a\b\c")); // '..\' is not going to be used
+            ClassicAssert.AreEqual(@"b\c", PathUtils.GetRelativePath(@"C:\a", @"C:\a\b\c"));
             ClassicAssert.AreEqual(@".", PathUtils.GetRelativePath(@"C:\a\b\c", @"C:\a\b\c"));
         }
     }
