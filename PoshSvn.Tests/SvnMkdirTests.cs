cf1f3382999c45bc23c0e672d6452cdb51960e70
rinrab <tima@chemodax.net>
2024-02-20 18:06:50 +0100

    Add more tests on svn-mkdir

--- PoshSvn.Tests/SvnMkdirTests.cs
+++ PoshSvn.Tests/SvnMkdirTests.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Management.Automation;
 using NUnit.Framework;
 using NUnit.Framework.Legacy;
 using PoshSvn.CmdLets;
 using PoshSvn.Tests.TestUtils;
+using SharpSvn;
 
 namespace PoshSvn.Tests
 {
@@ -45,6 +47,57 @@ namespace PoshSvn.Tests
                     Array.ConvertAll(actual.ToArray(), a => (string)a.BaseObject));
 
                 actual = sb.RunScript($"cd wc; svn-commit -m test");
+            }
+        }
+
+        [Test]
+        public void LocalMkdirOutputTest()
+        {
+            using (var sb = new WcSandbox())
+            {
+                var actual = sb.RunScript($@"svn-mkdir wc\test");
+
+                PSObjectAssert.AreEqual(
+                    new[]
+                    {
+                        new SvnMkdirOutput
+                        {
+                            Path = Path.Combine(sb.WcPath, "test")
+                        }
+                    },
+                    actual);
+            }
+        }
+
+        [Test]
+        public void MkdirWithParents()
+        {
+            using (var sb = new WcSandbox())
+            {
+                var actual = sb.RunScript($@"svn-mkdir wc\a\b\c -parent");
+
+                PSObjectAssert.AreEqual(
+                    new[]
+                    {
+                        new SvnMkdirOutput
+                        {
+                            Action = SharpSvn.SvnNotifyAction.Add,
+                            Path = Path.Combine(sb.WcPath, @"a")
+                        },
+                        new SvnMkdirOutput
+                        {
+                            Action = SharpSvn.SvnNotifyAction.Add,
+                            Path = Path.Combine(sb.WcPath, @"a\b")
+                        },
+                        new SvnMkdirOutput
+                        {
+                            Action = SharpSvn.SvnNotifyAction.Add,
+                            Path = Path.Combine(sb.WcPath, @"a\b\c")
+                        },
+                    },
+                    actual);
+
+                Assert.Throws<SvnSystemException>(() => sb.RunScript($@"svn-mkdir wc\a\b\c"));
             }
         }
 
