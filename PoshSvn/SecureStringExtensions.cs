05e6ea10daf7c37daffca46caa738b1cbce83efe
rinrab <tima@chemodax.net>
2024-02-25 19:41:04 +0100

    Implement security

---
+++ PoshSvn/SecureStringExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security;
+
+namespace PoshSvn
+{
+    public static class SecureStringExtensions
+    {
+        public static string AsPlainString(this SecureString secureString)
+        {
+            string result;
+            IntPtr bstr = IntPtr.Zero;
+
+            if (secureString == null)
+            {
+                return null;
+            }
+           
+            if(secureString.Length == 0)
+            {
+                return "";
+            }
+
+            try
+            {
+                bstr = Marshal.SecureStringToBSTR(secureString);
+                result = Marshal.PtrToStringBSTR(bstr);
+            }
+            finally
+            {
+                if (bstr != IntPtr.Zero)
+                {
+                    Marshal.ZeroFreeBSTR(bstr);
+                }
+            }
+
+            return result;
+        }
+    }
+}
