b54c77d90e0a7a683ab811d14e14dc2f58f04529
rinrab <tima@chemodax.net>
2024-03-07 16:06:54 +0100

    Use build script at github actions

---
+++ Build.ps1
@@ -0,0 +1,21 @@
+function Build-PoshSvn {
+    param (
+        [string]$Configuration = "Release"
+    )
+
+    $Output = "$PSScriptRoot\publish"
+    $ProjectPath = "$PSScriptRoot\PoshSvn\PoshSvn.csproj"
+
+    Remove-Item $Output -Recurse -Force
+
+    dotnet.exe build $ProjectPath --output $Output --configuration $Configuration -v=normal
+
+    Compress-Archive -Path $Output\* -DestinationPath "$Output.zip" -Force -CompressionLevel Optimal
+}
+
+function Test-PoshSvn {
+    param (
+    )
+
+    dotnet.exe test $PSScriptRoot --configuration Release --settings Default.runsettings -v=normal
+}
