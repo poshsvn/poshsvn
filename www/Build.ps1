e98429d49e735a113af76cd3b66524435c2cab93
rinrab <tima@chemodax.net>
2024-03-07 21:30:51 +0100

    Remove build website script.

--- www/Build.ps1
+++
@@ -1,9 +0,0 @@
-Set-Location $PSScriptRoot
-
-Remove-Item -Recurse -Force .\files -ErrorAction SilentlyContinue
-mkdir .\files
-
-dotnet msbuild clean ..
-dotnet msbuild .. /target:PoshSvn /p:Configuration=Release /p:Platform=x64
-
-Compress-Archive ..\bin\Release\* .\files\poshsvn-win-x64
