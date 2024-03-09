1828dd4b7ad447f828fc257a6475e2c76c71f1c7
rinrab <tima@chemodax.net>
2024-03-09 13:34:46 +0100

    Build-PoshSvn: Add building of documentation into MAML.

--- Scripts/Build-PoshSvn.ps1
+++ Scripts/Build-PoshSvn.ps1
@@ -9,4 +9,10 @@ Remove-Item $Output -Recurse -Force -ErrorAction Ignore
 
 dotnet.exe build $ProjectPath --output $Output --configuration $Configuration -v=normal
 
+if ($null -eq (Get-Module -ListAvailable -Name platyPS)) {
+    Install-Module -Name platyPS -Force
+}
+
+New-ExternalHelp -Path $PSScriptRoot\..\docs -OutputPath $Output\en-US -Force
+
 Compress-Archive -Path $Output\* -DestinationPath "$Output.zip" -Force -CompressionLevel Optimal
