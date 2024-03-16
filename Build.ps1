1a59ed5b3a8b8a70fffa7f2441993aa9cfaa58c6
rinrab <rinrab@0083e6b4-6090-234a-836e-88fcc467669c>
2024-03-16 21:35:51 +0000

    Move poshsvn source code into 'poshsvn' directory.
    git-svn-id: https://svn.rinrab.com/rinrab/poshsvn/tags/0.1.1@385 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ Build.ps1
@@ -0,0 +1,29 @@
+function Build-PoshSvn {
+    param (
+        [string]$Configuration = "Release"
+    )
+
+    $Output = "$PSScriptRoot\bin\poshsvn"
+    $ProjectPath = "$PSScriptRoot\PoshSvn\PoshSvn.csproj"
+
+    Remove-Item $Output -Recurse -Force -ErrorAction Ignore
+
+    dotnet.exe build $ProjectPath --output $Output --configuration $Configuration -v=normal
+
+    Compress-Archive -Path $Output\* -DestinationPath "$Output.zip" -Force -CompressionLevel Optimal
+}
+
+function Test-PoshSvn {
+    param (
+    )
+
+    dotnet.exe test $PSScriptRoot --configuration Release --settings Default.runsettings -v=normal
+}
+
+function Publish-PoshSvn {
+    param (
+        [string]$NuGetApiKey
+    )
+    
+    Publish-Module -Path "$PSScriptRoot\bin\poshsvn" -NuGetApiKey $NuGetApiKey
+}
---
+++ PoshSvn/CmdLets/SvnAdd.cs
@@ -0,0 +1,48 @@
+﻿using System.Management.Automation;
+using SharpSvn;
+
+namespace PoshSvn.CmdLets
+{
+    [Cmdlet("Invoke", "SvnAdd")]
+    [Alias("svn-add")]
+    [OutputType(typeof(SvnNotifyOutput))]
+    public class SvnAdd : SvnClientCmdletBase
+    {
+        [Parameter(Position = 0, Mandatory = true, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, ValueFromRemainingArguments = true)]
+        public string[] Path { get; set; }
+
+        [Parameter()]
+        public SvnDepth Depth { get; set; } = SvnDepth.Infinity;
+
+        [Parameter()]
+        public SwitchParameter Force { get; set; }
+
+        [Parameter()]
+        [Alias("no-ignore")]
+        public SwitchParameter NoIgnore { get; set; }
+
+        [Parameter()]
+        [Alias("no-auto-props")]
+        public SwitchParameter NoAutoProps { get; set; }
+
+        [Parameter()]
+        public SwitchParameter Parents { get; set; }
+
+        protected override void Execute()
+        {
+            SvnAddArgs args = new SvnAddArgs
+            {
+                Depth = Depth.ConvertToSharpSvnDepth(),
+                Force = Force,
+                NoIgnore = NoIgnore,
+                NoAutoProps = NoAutoProps,
+                AddParents = Parents,
+            };
+
+            foreach (string path in GetPathTargets(Path, null))
+            {
+                SvnClient.Add(path, args);
+            }
+        }
+    }
+}
