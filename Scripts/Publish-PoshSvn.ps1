fd683c1e90539f02313c34e1f2eb009f2a612d36
rinrab <rinrab@0083e6b4-6090-234a-836e-88fcc467669c>
2024-03-17 21:48:40 +0000

    Update publish script for using with subversion.
    git-svn-id: https://svn.rinrab.com/rinrab/poshsvn/trunk@417 0083e6b4-6090-234a-836e-88fcc467669c

--- Scripts/Publish-PoshSvn.ps1
+++ Scripts/Publish-PoshSvn.ps1
@@ -1,5 +1,36 @@
 param (
+    [Parameter(Mandatory)]
     [string]$NuGetApiKey
 )
-    
+
+# & "$PSScriptRoot\Build-PoshSvn"
+
+# & "$PSScriptRoot\Test-PoshSvn"
+
+$baseUrl = (svn-info "$PSScriptRoot\..").Url
+
+$data = Import-PowerShellDataFile -Path $PSScriptRoot\..\PoshSvn\PoshSvn.psd1
+$tagName = $data.ModuleVersion
+
+if (($baseUrl.Segments | Select-Object -Last 1) -eq "trunk/") {
+    $tagUrl = "$baseUrl/../tags/$tagName"
+    try {
+        svn-copy $baseUrl $tagUrl -maa "Create tag '$tagName'"
+    }
+    catch {
+        $choice = @(
+            (New-Object System.Management.Automation.Host.ChoiceDescription("&Yes", "Yes")),
+            (New-Object System.Management.Automation.Host.ChoiceDescription("&No", "No"))
+        )
+        $response = $Host.Ui.PromptForChoice($null, "Cannot create tag. Do you want to continue?", $choice, 0)
+
+        if ($response -ne 0) {
+            throw "Operation aborted."
+        }
+    }
+}
+else {
+    throw "Publishing is now supported only from 'trunk'"
+}
+
 Publish-Module -Path "$PSScriptRoot\..\bin\poshsvn" -NuGetApiKey $NuGetApiKey
