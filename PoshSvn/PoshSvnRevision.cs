16bb740391b4d3fbfd124ef86d5d765ce59906bf
rinrab <rinrab@0083e6b4-6090-234a-836e-88fcc467669c>
2024-03-18 16:02:14 +0000

    Add class PoshSvnRevision.
    git-svn-id: https://svn.rinrab.com/rinrab/poshsvn/branches/log-revision@436 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ PoshSvn/PoshSvnRevision.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+namespace PoshSvn
+{
+    public class PoshSvnRevision
+    {
+        public SvnRevisionType RevisionType { get; set; }
+        public long Revision { get; set; }
+
+        public PoshSvnRevision(long revision)
+        {
+            RevisionType = SvnRevisionType.Number;
+            Revision = revision;
+        }
+
+        public PoshSvnRevision(string str)
+        {
+            // TODO:
+        }
+
+        public PoshSvnRevision(SvnRevisionType type)
+        {
+            RevisionType = type;
+        }
+
+        public SharpSvn.SvnRevision ToSharpSvnRevision()
+        {
+            if (RevisionType == SvnRevisionType.Number)
+            {
+                return new SharpSvn.SvnRevision(Revision);
+            }
+            else
+            {
+                return new SharpSvn.SvnRevision(RevisionType.ToSharpSvnRevisionType());
+            }
+        }
+    }
+}
---
+++ PoshSvn/SvnRevisionTypeExtensions.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+using System;
+
+namespace PoshSvn
+{
+    public static class SvnRevisionTypeExtensions
+    {
+        public static SharpSvn.SvnRevisionType ToSharpSvnRevisionType(this SvnRevisionType revisionType)
+        {
+            switch (revisionType)
+            {
+                case SvnRevisionType.None:
+                    return SharpSvn.SvnRevisionType.None;
+
+                case SvnRevisionType.Number:
+                    return SharpSvn.SvnRevisionType.Number;
+
+                case SvnRevisionType.Time:
+                    return SharpSvn.SvnRevisionType.Time;
+
+                case SvnRevisionType.Committed:
+                    return SharpSvn.SvnRevisionType.Committed;
+
+                case SvnRevisionType.Previous:
+                    return SharpSvn.SvnRevisionType.Previous;
+
+                case SvnRevisionType.Base:
+                    return SharpSvn.SvnRevisionType.Base;
+
+                case SvnRevisionType.Working:
+                    return SharpSvn.SvnRevisionType.Working;
+
+                case SvnRevisionType.Head:
+                    return SharpSvn.SvnRevisionType.Head;
+
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
