bb6edb22279bd4c604cb3ef140946461486965d9
rinrab <rinrab@0083e6b4-6090-234a-836e-88fcc467669c>
2024-03-18 16:19:51 +0000

    Add class PoshSvnRevisionRange.
    git-svn-id: https://svn.rinrab.com/rinrab/poshsvn/branches/log-revision@438 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ PoshSvn/PoshSvnRevisionRange.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+namespace PoshSvn
+{
+    public class PoshSvnRevisionRange
+    {
+        public PoshSvnRevision EndRevision { get; set; }
+        public PoshSvnRevision StartRevision { get; set; }
+
+        public PoshSvnRevisionRange(PoshSvnRevision endRevision, PoshSvnRevision startRevision)
+        {
+            EndRevision = endRevision;
+            StartRevision = startRevision;
+        }
+    }
+}
