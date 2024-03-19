df24a77afdfda6aacca6165d5cb87e89ffafb050
rinrab <rinrab@0083e6b4-6090-234a-836e-88fcc467669c>
2024-03-19 11:55:01 +0000

    Implement all parameters of svn-export cmdlet.
    git-svn-id: https://svn.rinrab.com/rinrab/poshsvn/branches/import-export@461 0083e6b4-6090-234a-836e-88fcc467669c

--- PoshSvn/CmdLets/SvnExportCmdlet.cs
+++ PoshSvn/CmdLets/SvnExportCmdlet.cs
@@ -20,10 +20,29 @@ namespace PoshSvn.CmdLets
         [Alias("rev", "r")]
         public PoshSvnRevision Revision { get; set; }
 
+        [Parameter()]
+        public SvnDepth Depth { get; set; } = SvnDepth.Infinity;
+
+        [Parameter()]
+        [Alias("f")]
+        public SwitchParameter Force { get; set; }
+
+        [Parameter()]
+        [Alias("ignore-externals")]
+        public SwitchParameter IgnoreExternals { get; set; }
+
+        [Parameter()]
+        [Alias("ignore-keywords")]
+        public SwitchParameter IgnoreKeywords { get; set; }
+
         protected override void Execute()
         {
             SharpSvn.SvnExportArgs args = new SharpSvn.SvnExportArgs
             {
+                Depth = Depth.ConvertToSharpSvnDepth(),
+                Overwrite = Force,
+                IgnoreExternals = IgnoreExternals,
+                IgnoreKeywords = IgnoreKeywords
             };
 
             if (Revision != null)
