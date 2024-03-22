e229a32502d5e260ed54280bbe8c519806b65aab
rinrab <rinrab@0083e6b4-6090-234a-836e-88fcc467669c>
2024-03-22 15:02:42 +0000

    Create a terminal profile.
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/shelves/vscode@522 0083e6b4-6090-234a-836e-88fcc467669c

--- PoshSvn.vscode/src/extension.ts
+++ PoshSvn.vscode/src/extension.ts
@@ -1,5 +1,17 @@
 import * as vscode from 'vscode';
 
+function provideTerminalProfile(token: vscode.CancellationToken):
+    vscode.ProviderResult<vscode.TerminalProfile> {
+    return {
+        options: {
+            name: "PoshSvn terminal",
+            shellPath: "powershell",
+            shellArgs: `-NoExit -Command ipmo "${__dirname}\\PoshSvn.psd1"`,
+            // TODO: fill other parameters
+        }
+    };
+}
+
 export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.commands.registerCommand('PoshSvn.open.terminal', () => {
         let terminal = vscode.window.createTerminal(
@@ -10,6 +22,10 @@ export function activate(context: vscode.ExtensionContext) {
 
         terminal.show();
     }))
+
+    vscode.window.registerTerminalProfileProvider('PoshSvn.terminal.profile', {
+        provideTerminalProfile: provideTerminalProfile
+    });
 }
 
 export function deactivate() { }
