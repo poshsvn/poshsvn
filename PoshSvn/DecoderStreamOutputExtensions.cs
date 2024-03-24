b96a4d9be63e2eaf6da34aac73bb27a07d251043
rinrab <rinrab@0083e6b4-6090-234a-836e-88fcc467669c>
2024-03-24 14:53:19 +0000

    Add DecoderStreamOutputExtensions.
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/trunk@589 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ PoshSvn/DecoderStreamOutputExtensions.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+namespace PoshSvn
+{
+    public static class DecoderStreamOutputExtensions
+    {
+        public static void Write(this IDecoderStreamOutput output, string str)
+        {
+            char[] chars = str.ToCharArray();
+            output.Write(chars, 0, chars.Length);
+        }
+    }
+}
