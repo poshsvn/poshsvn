70ae5152c1b2e15ecd039fa80c508ea2188af7e3
rinrab <rinrab@0083e6b4-6090-234a-836e-88fcc467669c>
2024-03-24 14:24:21 +0000

    Add DecoderStream. Not used yet.
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/trunk@588 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ PoshSvn.Tests/DecoderStreamTests.cs
@@ -0,0 +1,132 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+using System.Text;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace PoshSvn.Tests
+{
+    [TestFixture]
+    public class DecoderStreamTests
+    {
+        private class TestDecoderStreamOutput : IDecoderStreamOutput
+        {
+            private readonly StringBuilder sb = new StringBuilder();
+
+            public void Write(char[] chars, int startIndex, int charCount)
+            {
+                sb.Append(chars, startIndex, charCount);
+            }
+
+            public void Dispose()
+            {
+            }
+
+            public string Content => sb.ToString();
+        }
+
+        [Test]
+        public void EmptyTest()
+        {
+            var writer = new TestDecoderStreamOutput();
+            using (var stream = new DecoderStream(writer, Encoding.UTF8))
+            {
+            }
+
+            ClassicAssert.AreEqual("", writer.Content);
+        }
+
+        [Test]
+        public void SimpleTest()
+        {
+            var writer = new TestDecoderStreamOutput();
+            using (var stream = new DecoderStream(writer, Encoding.UTF8))
+            {
+                stream.WriteByte((byte)'a');
+                stream.WriteByte((byte)'b');
+            }
+
+            ClassicAssert.AreEqual("ab", writer.Content);
+        }
+
+        [Test]
+        public void UTF8SequenceTest1()
+        {
+            var writer = new TestDecoderStreamOutput();
+            using (var stream = new DecoderStream(writer, Encoding.UTF8))
+            {
+                stream.WriteByte(0xE2);
+                stream.WriteByte(0x82);
+                stream.WriteByte(0xAC);
+            }
+
+            ClassicAssert.AreEqual("\u20AC", writer.Content);
+        }
+
+        [Test]
+        public void UTF8BOMTest()
+        {
+            var writer = new TestDecoderStreamOutput();
+            using (var stream = new DecoderStream(writer, Encoding.UTF8))
+            {
+                // UTF-8 BOM
+                stream.WriteByte(0xEF);
+                stream.WriteByte(0xBB);
+                stream.WriteByte(0xBF);
+
+                stream.WriteByte(0xE2);
+                stream.WriteByte(0x82);
+                stream.WriteByte(0xAC);
+            }
+
+            ClassicAssert.AreEqual("\uFEFF\u20AC", writer.Content);
+        }
+
+        [Test]
+        public void UTF8SequenceFlushTest()
+        {
+            var writer = new TestDecoderStreamOutput();
+            using (var stream = new DecoderStream(writer, Encoding.UTF8))
+            {
+                stream.WriteByte(0xE2);
+                stream.Flush();
+                stream.WriteByte(0x82);
+                stream.Flush();
+                stream.WriteByte(0xAC);
+            }
+
+            ClassicAssert.AreEqual("\u20AC", writer.Content);
+        }
+
+        [Test]
+        public void IncompleteUTF8SequenceTest()
+        {
+            var writer = new TestDecoderStreamOutput();
+            using (var stream = new DecoderStream(writer, Encoding.UTF8))
+            {
+                stream.WriteByte(0xE2);
+                stream.WriteByte(0x82);
+            }
+
+            ClassicAssert.AreEqual("\uFFFD", writer.Content);
+        }
+
+        [Test]
+        [TestCase("abdefedfdsfdsfsdfds")]
+        public void RoundTripUTF32Test(string text)
+        {
+            var writer = new TestDecoderStreamOutput();
+            using (var stream = new DecoderStream(writer, Encoding.UTF32))
+            {
+                var bytes = Encoding.UTF32.GetBytes(text);
+
+                foreach (var b in bytes)
+                {
+                    stream.WriteByte(b);
+                }
+            }
+
+            ClassicAssert.AreEqual(text, writer.Content);
+        }
+    }
+}
---
+++ PoshSvn/DecoderStream.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+using System;
+using System.IO;
+using System.Text;
+
+namespace PoshSvn
+{
+    public interface IDecoderStreamOutput : IDisposable
+    {
+        void Write(char[] chars, int startIndex, int charCount);
+    }
+
+    public class DecoderStream : Stream
+    {
+        private readonly IDecoderStreamOutput output;
+        private readonly Decoder decoder;
+
+        public DecoderStream(IDecoderStreamOutput output, 
+                             Encoding encoding)
+        {
+            this.output = output;
+            decoder = encoding.GetDecoder();
+        }
+
+        public override bool CanRead => false;
+
+        public override bool CanSeek => false;
+
+        public override bool CanWrite => true;
+
+        public override long Length => throw new NotSupportedException();
+
+        public override long Position { get => throw new NotSupportedException(); set => throw new NotSupportedException(); }
+
+        public override void Flush()
+        {
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            Process(buffer, offset, count, false);
+        }
+
+        public override void Close()
+        {
+            base.Close();
+            Process(Array.Empty<byte>(), 0, 0, true);
+            output.Dispose();
+        }
+
+        private void Process(byte[] buffer, int offset, int count, bool flush)
+        {
+            int charsCount = decoder.GetCharCount(buffer, offset, count, flush);
+
+            char[] chars = new char[charsCount];
+
+            charsCount = decoder.GetChars(buffer, offset, count, chars, 0, flush);
+
+            if (charsCount > 0)
+            { 
+                output.Write(chars, 0, charsCount);
+            }
+        }
+    }
+}
