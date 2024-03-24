86e8257a07b70b0e72f28c361d44ef1f719e38ff
rinrab <rinrab@0083e6b4-6090-234a-836e-88fcc467669c>
2024-03-24 18:20:25 +0000

    Add deleting of output directory before build.
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/trunk@601 0083e6b4-6090-234a-836e-88fcc467669c

--- Scripts/Build-PoshSvnVSCodeExtension.ps1
+++ Scripts/Build-PoshSvnVSCodeExtension.ps1
@@ -1,10 +1,13 @@
 $extensionRoot = "$PSScriptRoot\..\PoshSvn.vscode"
+$outDir = "$extensionRoot\out"
 
 Push-Location $extensionRoot
+
+Remove-Item -Recurse -Force $outDir -ErrorAction SilentlyContinue
 
 & npm install
 & tsc
 & "$PSScriptRoot\Build-PoshSvn.ps1"
-Copy-Item "$PSScriptRoot\..\bin\poshsvn\*" "$extensionRoot\out" -Recurse -Force
+Copy-Item "$PSScriptRoot\..\bin\poshsvn\*" $outDir -Recurse -Force
 
 Pop-Location
