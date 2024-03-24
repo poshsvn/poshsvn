54ddd40ccc41e0ee3af922ee9722cd0964bc3d5b
rinrab <rinrab@0083e6b4-6090-234a-836e-88fcc467669c>
2024-03-24 18:13:06 +0000

    Build vscode extension using script.
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/trunk@598 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ Scripts/Build-PoshSvnVSCodeExtension.ps1
@@ -0,0 +1,10 @@
+$extensionRoot = "$PSScriptRoot\..\PoshSvn.vscode"
+
+Push-Location $extensionRoot
+
+& npm install
+& tsc
+& "$PSScriptRoot\Build-PoshSvn.ps1"
+Copy-Item "$PSScriptRoot\..\bin\poshsvn\*" "$extensionRoot\out" -Recurse
+
+Pop-Location
