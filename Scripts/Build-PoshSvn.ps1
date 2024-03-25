0ec5cadfd1a2cdaad0571f9085f83eb3709afdaa
rinrab <rinrab@0083e6b4-6090-234a-836e-88fcc467669c>
2024-03-25 13:53:00 +0000

    Add restoring of nuget packages from build script.
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/trunk@614 0083e6b4-6090-234a-836e-88fcc467669c

--- Scripts/Build-PoshSvn.ps1
+++ Scripts/Build-PoshSvn.ps1
@@ -1,3 +1,4 @@
+[CmdletBinding()]
 param (
     [string]$Configuration = "Release"
 )
@@ -8,6 +9,10 @@ $SolutionPath = "$rootPath\PoshSvn.sln"
 
 Remove-Item $Output -Recurse -Force -ErrorAction Ignore
 
+Write-Verbose "Restoring nuget packages..."
+dotnet.exe msbuild $SolutionPath /t:PoshSvn -restore
+
+Write-Verbose "Building project..."
 dotnet.exe msbuild $SolutionPath /t:PoshSvn /p:Configuration=$Configuration /p:OutputPath=$Output /v:normal
 
 if ($null -eq (Get-Module -ListAvailable -Name platyPS)) {
