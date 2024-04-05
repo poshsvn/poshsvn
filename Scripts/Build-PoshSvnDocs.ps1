5318770d51cc878d7fc26049531a772fd4c0cd0b
rinrab <rinrab@0083e6b4-6090-234a-836e-88fcc467669c>
2024-04-05 12:57:54 +0000

    Little fix in documentation generation.
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/trunk@834 0083e6b4-6090-234a-836e-88fcc467669c

--- Scripts/Build-PoshSvnDocs.ps1
+++ Scripts/Build-PoshSvnDocs.ps1
@@ -22,5 +22,9 @@ else {
 }
 
 foreach ($path in Get-ChildItem $InputDir -Exclude obj -Directory) {
-    New-ExternalHelp -Path $path -OutputPath $OutputDir\$($path.Name) -Force
+    New-ExternalHelp -Path "$path\Cmdlets" -OutputPath "$OutputDir\$($path.Name)" -Force
+
+    Get-ChildItem $path -Exclude "Cmdlets" -Recurse -Directory | Get-ChildItem -Filter "*.md" | ForEach-Object {
+        New-ExternalHelp -Path $_ -OutputPath "$OutputDir\$($path.Name)" -Force
+    }
 }
