6a1b0f25a39df13ac8615ced042f7ffa3a367637
rinrab <rinrab@0083e6b4-6090-234a-836e-88fcc467669c>
2024-04-06 22:07:16 +0000

    On 'svn-merge-cmdlet': Add tests on output formatter.
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/branches/svn-merge-cmdlet@887 0083e6b4-6090-234a-836e-88fcc467669c

--- PoshSvn.Tests/SvnMergeCmdletTests.cs
+++ PoshSvn.Tests/SvnMergeCmdletTests.cs
@@ -2,6 +2,7 @@
 
 using System.IO;
 using NUnit.Framework;
+using NUnit.Framework.Legacy;
 using PoshSvn.Tests.TestUtils;
 
 namespace PoshSvn.Tests
@@ -23,6 +24,29 @@ namespace PoshSvn.Tests
                     {
                         new SvnNotifyOutput { Action = SvnNotifyAction.UpdateAdd, Path = Path.Combine(sb.TrunkPath, "dir") },
                         new SvnNotifyOutput { Action = SvnNotifyAction.RecordMergeInfo, Path = Path.Combine(sb.TrunkPath) },
+                    },
+                    actual);
+            }
+        }
+
+        [Test]
+        public void SimpleFormatTest()
+        {
+            using (var sb = new ProjectStructureSandbox())
+            {
+                sb.RunScript($@"svn-copy '{sb.ReposUrl}/trunk' '{sb.ReposUrl}/branches/test' -m branch");
+                sb.RunScript($@"svn-mkdir '{sb.ReposUrl}/branches/test/dir' -m test");
+                sb.RunScript($@"svn-update wc-trunk");
+                var actual = sb.FormatObject(sb.RunScript($@"svn-merge '{sb.ReposUrl}/branches/test' wc-trunk"), "Format-Custom");
+
+                CollectionAssert.AreEqual(
+                    new object[]
+                    {
+                        @"",
+                        @"A       wc-trunk\dir",
+                        @"U       wc-trunk",
+                        @"",
+                        @"",
                     },
                     actual);
             }
@@ -59,6 +83,31 @@ namespace PoshSvn.Tests
                     {
                         new SvnNotifyOutput { Action = SvnNotifyAction.TreeConflict, Path = Path.Combine(sb.TrunkPath, "a.txt") },
                         new SvnNotifyOutput { Action = SvnNotifyAction.RecordMergeInfo, Path = Path.Combine(sb.TrunkPath) },
+                    },
+                    actual);
+            }
+        }
+
+        [Test]
+        public void ResolveConflictByAcceptParameterFormatTest()
+        {
+            using (var sb = new ProjectStructureSandbox())
+            {
+                sb.RunScript($@"svn-copy '{sb.ReposUrl}/trunk' '{sb.ReposUrl}/branches/test' -m branch");
+                sb.RunScript($@"cd wc-trunk; 'abc' > a.txt; svn-add a.txt; svn-commit -m 'add a.txt'");
+                sb.RunScript($@"svn-switch '{sb.ReposUrl}/branches/test' wc-trunk");
+                sb.RunScript($@"cd wc-trunk; 'xyz' > a.txt; svn-add a.txt; svn-commit -m 'add a.txt'");
+
+                var actual = sb.FormatObject(sb.RunScript($@"svn-merge '{sb.ReposUrl}/branches/test' wc-trunk -Accept Postpone"), "Format-Custom");
+
+                CollectionAssert.AreEqual(
+                    new object[]
+                    {
+                        @"",
+                        @"C       wc-trunk\a.txt",
+                        @"U       wc-trunk",
+                        @"",
+                        @"",
                     },
                     actual);
             }
