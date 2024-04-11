e0a8562657acc8b00de30be047c834a6eaf44499
Timofei Zhakov <tima@chemodax.net>
2024-04-11 17:34:41 +0000

    Fix svn-merge tests.
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/branches/svn-merge-cmdlet@950 0083e6b4-6090-234a-836e-88fcc467669c

--- PoshSvn.Tests/SvnMergeCmdletTests.cs
+++ PoshSvn.Tests/SvnMergeCmdletTests.cs
@@ -68,7 +68,6 @@ namespace PoshSvn.Tests
         }
 
         [Test]
-        [Ignore("Bad test")]
         public void ResolveConflictByAcceptParameterTest()
         {
             using (var sb = new ProjectStructureSandbox())
@@ -79,7 +78,7 @@ namespace PoshSvn.Tests
                 sb.RunScript($@"cd wc-trunk; 'xyz' > a.txt; svn-add a.txt; svn-commit -m 'add a.txt'");
                 sb.RunScript($@"svn-update wc-trunk");
 
-                var actual = sb.RunScript($@"svn-merge '{sb.ReposUrl}/branches/test' wc-trunk -Accept Postpone");
+                var actual = sb.RunScript($@"svn-merge '{sb.ReposUrl}/trunk' wc-trunk -Accept Postpone");
 
                 PSObjectAssert.AreEqual(
                     new object[]
@@ -92,7 +91,6 @@ namespace PoshSvn.Tests
         }
 
         [Test]
-        [Ignore("Bad test")]
         public void ResolveConflictByAcceptParameterFormatTest()
         {
             using (var sb = new ProjectStructureSandbox())
@@ -103,7 +101,7 @@ namespace PoshSvn.Tests
                 sb.RunScript($@"cd wc-trunk; 'xyz' > a.txt; svn-add a.txt; svn-commit -m 'add a.txt'");
                 sb.RunScript($@"svn-update wc-trunk");
 
-                var actual = sb.FormatObject(sb.RunScript($@"svn-merge '{sb.ReposUrl}/branches/test' wc-trunk -Accept Postpone"), "Format-Custom");
+                var actual = sb.FormatObject(sb.RunScript($@"svn-merge '{sb.ReposUrl}/trunk' wc-trunk -Accept Postpone"), "Format-Custom");
 
                 //svn.exe does the following:
                 //---Merging r2 through r4 into '.':
@@ -118,6 +116,40 @@ namespace PoshSvn.Tests
                     {
                         @"",
                         @"C       wc-trunk\a.txt",
+                        @"U       wc-trunk",
+                        @"",
+                        @"",
+                    },
+                    actual);
+            }
+        }
+
+        [Test]
+        public void MergeMoveTest()
+        {
+            using (var sb = new ProjectStructureSandbox())
+            {
+                sb.RunScript($@"svn-copy '{sb.ReposUrl}/trunk' '{sb.ReposUrl}/branches/test' -m branch");
+                sb.RunScript($@"cd wc-trunk; 'abc' > a.txt; svn-add a.txt; svn-commit -m 'add a.txt'");
+                sb.RunScript($@"svn-switch '{sb.ReposUrl}/branches/test' wc-trunk");
+                sb.RunScript($@"svn-move '{sb.ReposUrl}/trunk/a.txt' '{sb.ReposUrl}/trunk/b.txt' -m move");
+                sb.RunScript($@"svn-update wc-trunk");
+
+                var actual = sb.FormatObject(sb.RunScript($@"svn-merge '{sb.ReposUrl}/trunk' wc-trunk -Accept Postpone"), "Format-Custom");
+
+                //svn.exe does the following:
+                //---Merging r2 through r4 into '.':
+                //   C a.txt
+                //--- Recording mergeinfo for merge of r2 through r4 into '.':
+                // U   .
+                //Summary of conflicts:
+                //  Tree conflicts: 1
+
+                CollectionAssert.AreEqual(
+                    new object[]
+                    {
+                        @"",
+                        @"A       wc-trunk\b.txt",
                         @"U       wc-trunk",
                         @"",
                         @"",
