105b79b3bc7d5f616212bd6adcecfb33cca8d89f
Timofei Zhakov <tima@chemodax.net>
2024-04-27 21:14:16 +0000

    Rework ResolvedTargetCollection:
    * Constructor: Parse and build Paths and Urls lists based on targets.
    * Constructor now takes IEnumerable of SvnResolvedTarget as parameter.
    * Capitalize Targets property (and convert it to property).
    
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/branches/svn-target-2.0@1274 0083e6b4-6090-234a-836e-88fcc467669c

--- PoshSvn/ResolvedTargetCollection.cs
+++ PoshSvn/ResolvedTargetCollection.cs
@@ -1,21 +1,40 @@
 ﻿// Copyright (c) Timofei Zhakov. All rights reserved.
 
+using System;
 using System.Collections.Generic;
 
 namespace PoshSvn
 {
     public class ResolvedTargetCollection
     {
-        private readonly ICollection<SvnResolvedTarget> targets;
+        public List<SvnResolvedTarget> Targets;
+        public List<string> Paths { get; }
+        public List<Uri> Urls { get; }
 
-        public ResolvedTargetCollection(ICollection<SvnResolvedTarget> targets)
+        public ResolvedTargetCollection(IEnumerable<SvnResolvedTarget> targets)
         {
-            this.targets = targets;
+            Targets = new List<SvnResolvedTarget>();
+            Paths = new List<string>();
+            Urls = new List<Uri>();
+
+            foreach (SvnResolvedTarget target in targets)
+            {
+                if (target.TryGetPath(out string path))
+                {
+                    Paths.Add(path);
+                }
+                else if (target.TryGetUrl(out Uri url))
+                {
+                    Urls.Add(url);
+                }
+
+                Targets.Add(target);
+            }
         }
 
         public IEnumerable<SharpSvn.SvnTarget> EnumerateSharpSvnTargets()
         {
-            foreach (SvnResolvedTarget target in targets)
+            foreach (SvnResolvedTarget target in Targets)
             {
                 yield return target.ConvertToSharpSvnTarget();
             }
@@ -25,7 +44,7 @@ namespace PoshSvn
         {
             List<SharpSvn.SvnTarget> rv = new List<SharpSvn.SvnTarget>();
 
-            foreach (SvnResolvedTarget target in targets)
+            foreach (SvnResolvedTarget target in Targets)
             {
                 rv.Add(target.ConvertToSharpSvnTarget());
             }
