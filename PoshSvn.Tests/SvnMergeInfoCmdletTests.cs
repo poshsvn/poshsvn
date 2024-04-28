c3ef855d0c7d92322d0e9813c105459cfa2907bf
Timofei Zhakov <tima@chemodax.net>
2024-04-28 16:33:01 +0000

    Add EligibleTestNone test.
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/branches/svn-mergeinfo-cmdlet@1305 0083e6b4-6090-234a-836e-88fcc467669c

--- PoshSvn.Tests/SvnMergeInfoCmdletTests.cs
+++ PoshSvn.Tests/SvnMergeInfoCmdletTests.cs
@@ -33,6 +33,29 @@ namespace PoshSvn.Tests
                     actual);
             }
         }
+        [Test]
+        public void EligibleTestNone()
+        {
+            using (var sb = new ProjectStructureSandbox())
+            {
+                sb.RunScript($@"svn-copy '{sb.ReposUrl}/trunk' '{sb.ReposUrl}/branches/test' -m branch");
+                sb.RunScript($@"cd wc-trunk; 'abc' > a.txt; svn-add a.txt; svn-commit -m 'add a.txt'");
+                sb.RunScript($@"cd wc-trunk; 'abc' > b.txt; svn-add b.txt; svn-commit -m 'add b.txt'");
+                sb.RunScript($@"cd wc-trunk; 'abc' > c.txt; svn-add c.txt; svn-commit -m 'add c.txt'");
+                sb.RunScript($@"cd wc-trunk; 'xyz' > x.txt; svn-add x.txt; svn-commit -m 'add x.txt'");
+                sb.RunScript($@"cd wc-trunk; 'xyz' > y.txt; svn-add y.txt; svn-commit -m 'add y.txt'");
+
+                var actual = sb.RunScript($@"svn-mergeinfo '{sb.ReposUrl}/trunk' '{sb.ReposUrl}/branches/test' -ShowRevs Eligible");
+
+                PSObjectAssert.AreEqual(
+                    new object[]
+                    {
+                        // What???
+                        new SvnMergeInfo { Revision = 2 },
+                    },
+                    actual);
+            }
+        }
 
         [Test]
         public void EligibleSomeTest()
