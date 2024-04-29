271fb9b3f67aa8bec31c003e1f28f0e787cafd19
Timofei Zhakov <tima@chemodax.net>
2024-04-29 22:15:29 +0000

    Add Format-List formatter for SvnProperty and tests on it.
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/branches/props@1403 0083e6b4-6090-234a-836e-88fcc467669c

--- PoshSvn.Tests/SvnPropertyTests.cs
+++ PoshSvn.Tests/SvnPropertyTests.cs
@@ -88,6 +88,137 @@ namespace PoshSvn.Tests
         }
 
         [Test]
+        public void FormatListTest()
+        {
+            using (var sb = new WcSandbox())
+            {
+                var actual = sb.FormatObject(new object[]
+                {
+                    new SvnProperty
+                    {
+                        Name = "name",
+                        Value = "value",
+                        Path = Path.Combine(sb.WcPath, "test"),
+                    },
+                    new SvnProperty
+                    {
+                        Name = "foo",
+                        Value = "bar",
+                        Path = Path.Combine(sb.WcPath, "test"),
+                    },
+                    new SvnProperty
+                    {
+                        Name = "svn:mergeinfo",
+                        Path = Path.Combine(sb.WcPath, "test"),
+                        Value =
+                            "/subversion/branches/resolve-incoming-add:1762797-1764284\n" +
+                            "/subversion/branches/revprop-cache:1298521-1326293\n" +
+                            "/subversion/branches/revprop-caching-ng:1620597,1620599\n" +
+                            "/subversion/branches/revprop-packing:1143907,1143971,1143997,1144017,1144499,1144568,1146145\n" +
+                            "/subversion/branches/shelve:1802592-1815226\n",
+                    },
+                },
+                "Format-List");
+
+                ClassicAssert.AreEqual(
+                    new object[]
+                    {
+                        @"",
+                        @"",
+                        @"    Properties on 'wc\test':",
+                        @"",
+                        @"",
+                        @"Name  : name",
+                        @"Value : value",
+                        @"",
+                        @"Name  : foo",
+                        @"Value : bar",
+                        @"",
+                        @"Name  : svn:mergeinfo",
+                        @"Value : /subversion/branches/resolve-incoming-add:1762797-1764284",
+                        @"        /subversion/branches/revprop-cache:1298521-1326293",
+                        @"        /subversion/branches/revprop-caching-ng:1620597,1620599",
+                        @"        /subversion/branches/revprop-packing:1143907,1143971,1143997,1144017,1144499,1144568,1146145",
+                        @"        /subversion/branches/shelve:1802592-1815226",
+                        @"",
+                        @"",
+                        @"",
+                        @"",
+                    },
+                    actual);
+            }
+        }
+
+        [Test]
+        public void FormatListManyGroupsTest()
+        {
+            using (var sb = new WcSandbox())
+            {
+                var actual = sb.FormatObject(new object[]
+                {
+                    new SvnProperty
+                    {
+                        Name = "svn:ignore",
+                        Value = "bin\nobj\n.vs\n",
+                        Path = Path.Combine(sb.WcPath),
+                    },
+                    new SvnProperty
+                    {
+                        Name = "svn:mergeinfo",
+                        Value = "/subversion/branches/resolve-incoming-add:1762797-1764284",
+                        Path = Path.Combine(sb.WcPath),
+                    },
+                    new SvnProperty
+                    {
+                        Name = "svn:ignore",
+                        Value = "bin\nobj\n.vs\nx64\nx86\n",
+                        Path = Path.Combine(sb.WcPath, "Project1"),
+                    },
+                    new SvnProperty
+                    {
+                        Name = "foo",
+                        Value = "this is a foo value!!",
+                        Path = Path.Combine(sb.WcPath, "test"),
+                    },
+                    new SvnProperty
+                    {
+                        Name = "bar",
+                        Value = "this is a bar value!!1!",
+                        Path = Path.Combine(sb.WcPath, "test"),
+                    },
+                },
+                "Format-List");
+
+                ClassicAssert.AreEqual(
+                    new object[]
+                    {
+                        @"",
+                        @"",
+                        @"    Properties on 'wc\test':",
+                        @"",
+                        @"",
+                        @"Name  : name",
+                        @"Value : value",
+                        @"",
+                        @"Name  : foo",
+                        @"Value : bar",
+                        @"",
+                        @"Name  : svn:mergeinfo",
+                        @"Value : /subversion/branches/resolve-incoming-add:1762797-1764284",
+                        @"        /subversion/branches/revprop-cache:1298521-1326293",
+                        @"        /subversion/branches/revprop-caching-ng:1620597,1620599",
+                        @"        /subversion/branches/revprop-packing:1143907,1143971,1143997,1144017,1144499,1144568,1146145",
+                        @"        /subversion/branches/shelve:1802592-1815226",
+                        @"",
+                        @"",
+                        @"",
+                        @"",
+                    },
+                    actual);
+            }
+        }
+
+        [Test]
         public void ManyGroups()
         {
             using (var sb = new WcSandbox())
