f60ed66e3d841be9df2175f3aa5469a0665a353e
Timofei Zhakov <tima@chemodax.net>
2024-04-29 18:36:04 +0000

    Move common SvnProperty tests to the SvnPropertyTests file.
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/branches/props@1385 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ PoshSvn.Tests/SvnPropertyTests.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+using System.IO;
+using NUnit.Framework;
+using PoshSvn.Tests.TestUtils;
+
+namespace PoshSvn.Tests
+{
+    public class SvnPropertyTests
+    {
+        [Test]
+        public void SimpleFormatTest()
+        {
+            using (var sb = new WcSandbox())
+            {
+                sb.RunScript(@"svn-mkdir wc\test");
+                sb.RunScript(@"svn-commit wc -m test");
+                sb.RunScript(@"svn-propset name value wc\test");
+                var actual = sb.RunScript(@"svn-propset name value2 wc\test");
+
+                PSObjectAssert.AreEqual(
+                    new object[]
+                    {
+                        new SvnProperty
+                        {
+                            Name = "name",
+                            Value = null,
+                            Path = Path.Combine(sb.WcPath, "test"),
+                        }
+                    },
+                    actual);
+            }
+        }
+    }
+}
