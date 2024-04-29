e4563743048c900d80bdac2742994f94261f9612
Timofei Zhakov <tima@chemodax.net>
2024-04-29 18:38:54 +0000

    Remove OutputFormatTableTest; it was moved to SvnPropertyTests class.
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/branches/props@1388 0083e6b4-6090-234a-836e-88fcc467669c

--- PoshSvn.Tests/SvnPropsetCmdletTests.cs
+++ PoshSvn.Tests/SvnPropsetCmdletTests.cs
@@ -78,28 +78,5 @@ namespace PoshSvn.Tests
                     actual);
             }
         }
-
-        [Test]
-        public void OutputFormatTableTest()
-        {
-            using (var sb = new WcSandbox())
-            {
-                sb.RunScript(@"svn-mkdir wc\test");
-                sb.RunScript(@"svn-commit wc -m test");
-                var actual = sb.FormatObject(sb.RunScript(@"svn-propset name value wc\test"), "Format-Table");
-
-                ClassicAssert.AreEqual(
-                    new object[]
-                    {
-                        @"",
-                        @"Name Value Path",
-                        @"---- ----- ----",
-                        @"name       wc\test",
-                        @"",
-                        @"",
-                    },
-                    actual);
-            }
-        }
     }
 }
