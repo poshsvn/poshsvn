4a3511c7abb507dec35cf13c3805835603ca7bfc
Timofei Zhakov <tima@chemodax.net>
2024-04-30 11:58:41 +0000

    Add tests on remote svn-proplist operation.
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/branches/props@1407 0083e6b4-6090-234a-836e-88fcc467669c

--- PoshSvn.Tests/SvnProplistCmdletTests.cs
+++ PoshSvn.Tests/SvnProplistCmdletTests.cs
@@ -35,6 +35,124 @@ namespace PoshSvn.Tests
         }
 
         [Test]
+        public void RemoteTest()
+        {
+            using (var sb = new WcSandbox())
+            {
+                sb.RunScript(@"svn-mkdir wc\test");
+                sb.RunScript(@"svn-commit wc -m test");
+                sb.RunScript(@"svn-propset name value wc\test");
+                sb.RunScript(@"svn-commit wc -m 'setup props'");
+
+                var actual = sb.RunScript($@"svn-proplist {sb.ReposUrl}/test");
+
+                PSObjectAssert.AreEqual(
+                    new object[]
+                    {
+                        new SvnProperty
+                        {
+                            Name = "name",
+                            Value = "value",
+                            Path = $"{sb.ReposUrl}/test",
+                        }
+                    },
+                    actual);
+            }
+        }
+
+        [Test]
+        public void RemoteManyNonRecurseTest()
+        {
+            using (var sb = new WcSandbox())
+            {
+                sb.RunScript(@"svn-mkdir wc\Project1");
+                sb.RunScript(@"svn-mkdir wc\test");
+                sb.RunScript(@"svn-propset svn:ignore ""bin`nobj`n.vs`n"" wc");
+                sb.RunScript(@"svn-propset 'svn:mergeinfo' '/subversion/branches/resolve-incoming-add:1762797-1764284' wc");
+                sb.RunScript(@"svn-propset svn:ignore ""bin`nobj`n.vs`nx64`nx86"" wc\Project1");
+                sb.RunScript(@"svn-propset foo 'this is a foo value!!' wc\test");
+                sb.RunScript(@"svn-propset bar 'this is a bar value!!1!' wc\test");
+
+                sb.RunScript(@"svn-commit wc -m 'setup props'");
+
+                var actual = sb.RunScript($@"svn-proplist {sb.ReposUrl} | Sort-Object -Property Name");
+
+                PSObjectAssert.AreEqual(
+                    new object[]
+                    {
+                        new SvnProperty
+                        {
+                            Name = "svn:ignore",
+                            Value = "bin\r\nobj\r\n.vs\r\n",
+                            Path = $"{sb.ReposUrl}",
+                        },
+                        new SvnProperty
+                        {
+                            Name = "svn:mergeinfo",
+                            Value = "/subversion/branches/resolve-incoming-add:1762797-1764284",
+                            Path = $"{sb.ReposUrl}",
+                        },
+                    },
+                    actual);
+            }
+        }
+
+        [Test]
+        public void RemoteManyRecurseTest()
+        {
+            using (var sb = new WcSandbox())
+            {
+                sb.RunScript(@"svn-mkdir wc\Project1");
+                sb.RunScript(@"svn-mkdir wc\test");
+                sb.RunScript(@"svn-propset svn:ignore ""bin`nobj`n.vs`n"" wc");
+                sb.RunScript(@"svn-propset 'svn:mergeinfo' '/subversion/branches/resolve-incoming-add:1762797-1764284' wc");
+                sb.RunScript(@"svn-propset svn:ignore ""bin`nobj`n.vs`nx64`nx86"" wc\Project1");
+                sb.RunScript(@"svn-propset foo 'this is a foo value!!' wc\test");
+                sb.RunScript(@"svn-propset bar 'this is a bar value!!1!' wc\test");
+
+                sb.RunScript(@"svn-commit wc -m 'setup props'");
+
+                var actual = sb.RunScript($@"svn-proplist {sb.ReposUrl} -Depth Infinity | Sort-Object -Property Name");
+
+                PSObjectAssert.AreEqual(
+                    new object[]
+                    {
+                        new SvnProperty
+                        {
+                            Name = "bar",
+                            Value = "this is a bar value!!1!",
+                            Path = $"{sb.ReposUrl}/test",
+                        },
+                        new SvnProperty
+                        {
+                            Name = "foo",
+                            Value = "this is a foo value!!",
+                            Path = $"{sb.ReposUrl}/test",
+                        },
+                        new SvnProperty
+                        {
+                            Name = "svn:ignore",
+                            Value = "bin\r\nobj\r\n.vs\r\n",
+                            Path = $"{sb.ReposUrl}",
+                        },
+                        new SvnProperty
+                        {
+                            Name = "svn:ignore",
+                            Value = "bin\r\nobj\r\n.vs\r\nx64\r\nx86\r\n",
+                            Path = $"{sb.ReposUrl}/Project1",
+                        },
+                        new SvnProperty
+                        {
+                            Name = "svn:mergeinfo",
+                            Value = "/subversion/branches/resolve-incoming-add:1762797-1764284",
+                            Path = $"{sb.ReposUrl}",
+                        },
+                    },
+                    actual);
+            }
+        }
+
+        [Test]
         public void SimpleTest2()
         {
             using (var sb = new WcSandbox())
