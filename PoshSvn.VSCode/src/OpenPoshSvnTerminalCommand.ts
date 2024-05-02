7e153cc4451ca28fe7e6b7f9c69c49068e723f8b
Timofei Zhakov <tima@chemodax.net>
2024-05-02 19:09:28 +0000

    Move the OpenPoshSvnTerminalCommand class to the OpenPoshSvnTerminalCommand.ts file.
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/trunk@1502 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ PoshSvn.VSCode/src/OpenPoshSvnTerminalCommand.ts
@@ -0,0 +1,20 @@
+﻿import * as vscode from 'vscode';
+import { terminalOptions } from './terminalProvider';
+
+
+export class OpenPoshSvnTerminalCommand implements vscode.Disposable {
+    private command: vscode.Disposable;
+
+    constructor() {
+        this.command = vscode.commands.registerCommand("PoshSvn.openTerminal", this.invokeCommand);
+    }
+
+    private invokeCommand() {
+        let terminal = vscode.window.createTerminal(terminalOptions);
+        terminal.show();
+    }
+
+    public dispose(): void {
+        this.command.dispose();
+    }
+}
--- PoshSvn.VSCode/src/extension.ts
+++ PoshSvn.VSCode/src/extension.ts
@@ -1,26 +1,10 @@
 ﻿import * as vscode from 'vscode';
 
 import { ILogger, Logger } from './logging';
-import { PoshSvnTerminalProfileProvider, terminalOptions } from './terminalProvider';
+import { PoshSvnTerminalProfileProvider } from './terminalProvider';
+import { OpenPoshSvnTerminalCommand } from './OpenPoshSvnTerminalCommand';
 
 const logger: ILogger = new Logger();
-
-export class OpenPoshSvnTerminalCommand implements vscode.Disposable {
-    private command: vscode.Disposable;
-
-    constructor() {
-        this.command = vscode.commands.registerCommand("PoshSvn.openTerminal", this.invokeCommand);
-    }
-
-    private invokeCommand() {
-        let terminal = vscode.window.createTerminal(terminalOptions);
-        terminal.show();
-    }
-
-    public dispose(): void {
-        this.command.dispose();
-    }
-}
 
 export function activate(context: vscode.ExtensionContext) {
     logger.log("Activating extension.");
