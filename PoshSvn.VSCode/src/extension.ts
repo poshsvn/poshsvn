064eab6754dda872e94db71e0c131b1f7c5cc296
Timofei Zhakov <tima@chemodax.net>
2024-05-02 19:08:31 +0000

    Factor-out invokeCommand() function.
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/trunk@1501 0083e6b4-6090-234a-836e-88fcc467669c

--- PoshSvn.VSCode/src/extension.ts
+++ PoshSvn.VSCode/src/extension.ts
@@ -9,10 +9,12 @@ export class OpenPoshSvnTerminalCommand implements vscode.Disposable {
     private command: vscode.Disposable;
 
     constructor() {
-        this.command = vscode.commands.registerCommand("PoshSvn.openTerminal", () => {
-            let terminal = vscode.window.createTerminal(terminalOptions);
-            terminal.show();
-        });
+        this.command = vscode.commands.registerCommand("PoshSvn.openTerminal", this.invokeCommand);
+    }
+
+    private invokeCommand() {
+        let terminal = vscode.window.createTerminal(terminalOptions);
+        terminal.show();
     }
 
     public dispose(): void {
