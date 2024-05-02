7fb9fb7be535664ec775c93434029feb00b04435
Timofei Zhakov <tima@chemodax.net>
2024-05-02 19:48:45 +0000

    PoshSvn.openTerminal command: Do not create new terminal if it is already exists.
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/trunk@1504 0083e6b4-6090-234a-836e-88fcc467669c

--- PoshSvn.VSCode/src/OpenPoshSvnTerminalCommand.ts
+++ PoshSvn.VSCode/src/OpenPoshSvnTerminalCommand.ts
@@ -8,7 +8,23 @@ export class OpenPoshSvnTerminalCommand extends CommandBase {
     }
 
     invokeCommand(): void {
-        let terminal = vscode.window.createTerminal(terminalOptions);
-        terminal.show();
+        const oldTerminal = OpenPoshSvnTerminalCommand.findFirstPoshsvnTerminal();
+
+        if (oldTerminal) {
+            oldTerminal.show();
+        } else {
+            let terminal = vscode.window.createTerminal(terminalOptions);
+            terminal.show();
+        }
+    }
+
+    static findFirstPoshsvnTerminal(): vscode.Terminal | null {
+        for (const terminal of vscode.window.terminals) {
+            if (terminal.name == "PoshSvn terminal") {
+                return terminal;
+            }
+        }
+
+        return null;
     }
 }
