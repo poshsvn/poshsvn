b613393f23937a55c6b14b856816b09d46141a70
Timofei Zhakov <tima@chemodax.net>
2024-05-05 11:44:05 +0000

    svn-blame: Added custom Format-List formatter for the svn-blame cmdlet.
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/trunk@1572 0083e6b4-6090-234a-836e-88fcc467669c

--- PoshSvn.Tests/SvnBlameCmdletTests.cs
+++ PoshSvn.Tests/SvnBlameCmdletTests.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Timofei Zhakov. All rights reserved.
 
+using System;
 using NUnit.Framework;
 using NUnit.Framework.Legacy;
 using PoshSvn.Tests.TestUtils;
@@ -53,6 +54,129 @@ namespace PoshSvn.Tests
                     actual,
                     nameof(SvnBlameLine.Author),
                     nameof(SvnBlameLine.Time));
+            }
+        }
+
+        [Test]
+        public void FormatListTest()
+        {
+            using (var sb = new WcSandbox())
+            {
+                sb.RunScript(@"Set-Content -Path wc\a.txt -Value line1,line2");
+                sb.RunScript(@"svn-add wc\a.txt");
+                sb.RunScript(@"svn-commit wc -m test");
+
+                sb.RunScript(@"Set-Content -Path wc\a.txt -Value line1,line2,line3");
+                sb.RunScript(@"svn-commit wc -m test");
+
+                sb.RunScript(@"Set-Content -Path wc\a.txt -Value line1,'modified line2',line3");
+                sb.RunScript(@"svn-commit wc -m test");
+
+                var actual = sb.FormatObject(
+                    new[]
+                    {
+                        new SvnBlameLine
+                        {
+                            Revision = 1,
+                            LineNumber = 0,
+                            Line = "line1",
+                            Author = "sally",
+                            Time = DateTime.Parse("5/5/2024 11:39:18 AM")
+                        },
+                        new SvnBlameLine
+                        {
+                            Revision = 3,
+                            LineNumber = 1,
+                            Line = "modified line2",
+                            Author = "harry",
+                            Time = DateTime.Parse("5/5/2024 11:39:18 AM")
+                        },
+                        new SvnBlameLine
+                        {
+                            Revision = 2,
+                            LineNumber = 2,
+                            Line = "line3",
+                            Author = "sally",
+                            Time = DateTime.Parse("5/5/2024 11:39:18 AM")
+                        },
+                        new SvnBlameLine
+                        {
+                            Revision = 5,
+                            LineNumber = 3,
+                            Line = "line4",
+                            Author = "John.Doe",
+                            Time = DateTime.Parse("5/5/2024 11:39:18 AM")
+                        },
+                        new SvnBlameLine
+                        {
+                            Revision = 5,
+                            LineNumber = 4,
+                            Line = "line5",
+                            Author = "John.Doe",
+                            Time = DateTime.Parse("5/5/2024 11:39:18 AM")
+                        },
+                    },
+                    "Format-List");
+
+                CollectionAssert.AreEqual(
+                    new object[]
+                    {
+                        "",
+                        "",
+                        "Revision           : 1",
+                        "Author             : sally",
+                        "Line               : line1",
+                        "RevisionProperties :",
+                        "LocalChange        : False",
+                        "Time               : 5/5/2024 11:39:18 AM",
+                        "LineNumber         : 0",
+                        "EndRevision        : 0",
+                        "StartRevision      : 0",
+                        "",
+                        "Revision           : 3",
+                        "Author             : harry",
+                        "Line               : modified line2",
+                        "RevisionProperties :",
+                        "LocalChange        : False",
+                        "Time               : 5/5/2024 11:39:18 AM",
+                        "LineNumber         : 1",
+                        "EndRevision        : 0",
+                        "StartRevision      : 0",
+                        "",
+                        "Revision           : 2",
+                        "Author             : sally",
+                        "Line               : line3",
+                        "RevisionProperties :",
+                        "LocalChange        : False",
+                        "Time               : 5/5/2024 11:39:18 AM",
+                        "LineNumber         : 2",
+                        "EndRevision        : 0",
+                        "StartRevision      : 0",
+                        "",
+                        "Revision           : 5",
+                        "Author             : John.Doe",
+                        "Line               : line4",
+                        "RevisionProperties :",
+                        "LocalChange        : False",
+                        "Time               : 5/5/2024 11:39:18 AM",
+                        "LineNumber         : 3",
+                        "EndRevision        : 0",
+                        "StartRevision      : 0",
+                        "",
+                        "Revision           : 5",
+                        "Author             : John.Doe",
+                        "Line               : line5",
+                        "RevisionProperties :",
+                        "LocalChange        : False",
+                        "Time               : 5/5/2024 11:39:18 AM",
+                        "LineNumber         : 4",
+                        "EndRevision        : 0",
+                        "StartRevision      : 0",
+                        "",
+                        "",
+                        "",
+                    },
+                    actual);
             }
         }
 
