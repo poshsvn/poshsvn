08e39481b821a1c5c98b31cc790ee799cc4f8030
Timofei Zhakov <tima@chemodax.net>
2024-05-05 18:14:44 +0000

    Use pre-compiled headers.
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/branches/svnjs-shelve@1597 0083e6b4-6090-234a-836e-88fcc467669c

--- svnjs/hello.cpp
+++ svnjs/hello.cpp
@@ -1,8 +1,4 @@
-#include <napi.h>
-
-#include <svn_client.h>
-#include <svn_pools.h>
-#include <svn_dirent_uri.h>
+#include "pch.h"
 
 typedef struct info_receiver_baton_t
 {
---
+++ svnjs/pch.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <napi.h>
+
+#include <svn_client.h>
+#include <svn_pools.h>
+#include <svn_dirent_uri.h>
