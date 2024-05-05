4b151e35b5a651077b17a45978b9a65c83067651
Timofei Zhakov <tima@chemodax.net>
2024-05-05 18:07:25 +0000

    Add info receiver and callback.
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/branches/svnjs-shelve@1596 0083e6b4-6090-234a-836e-88fcc467669c

--- svnjs/hello.cpp
+++ svnjs/hello.cpp
@@ -4,14 +4,41 @@
 #include <svn_pools.h>
 #include <svn_dirent_uri.h>
 
+typedef struct info_receiver_baton_t
+{
+    Napi::Function callback;
+    Napi::Env env;
+} info_receiver_baton_t;
+
+static svn_error_t*
+info_receiver(void* baton,
+              const char* target,
+              const svn_client_info2_t* info,
+              apr_pool_t* pool)
+{
+    info_receiver_baton_t* const receiver_baton = (info_receiver_baton_t*)baton;
+    Napi::Env env = receiver_baton->env;
+    Napi::Function callback = receiver_baton->callback;
+
+    callback.Call(env.Global(), { Napi::String::New(env, info->repos_root_URL) });
+
+    return svn_error_create(1, NULL, NULL);
+}
+
 Napi::String Info(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
+
+    Napi::Function callback = info[0].As<Napi::Function>();
+
+    info_receiver_baton_t receiver_baton = { callback, env };
 
     apr_pool_t* pool;
     apr_pool_create(&pool, NULL);
 
     svn_client_ctx_t* ctx;
     svn_client_create_context2(&ctx, NULL, pool);
+
+    svn_client_info_receiver2_t receiver = info_receiver;
 
     svn_opt_revision_t rev;
     memset(&rev, 0, sizeof(rev));
@@ -30,8 +57,8 @@ Napi::String Info(const Napi::CallbackInfo& info) {
         FALSE,
         FALSE,
         NULL,
-        NULL,
-        NULL,
+        info_receiver,
+        &receiver_baton,
         ctx,
         pool);
 
