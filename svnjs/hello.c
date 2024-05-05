c27a05033c28f61f05e8672a8278dbd19b4a5a6a
Timofei Zhakov <tima@chemodax.net>
2024-05-05 17:38:21 +0000

    Invoke Subversion info command.
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/branches/svnjs-shelve@1591 0083e6b4-6090-234a-836e-88fcc467669c

--- svnjs/hello.c
+++ svnjs/hello.c
@@ -1,7 +1,42 @@
 #include <assert.h>
 #include <node_api.h>
+#include <svn_client.h>
+#include <svn_pools.h>
+#include <svn_dirent_uri.h>
 
 static napi_value Method(napi_env env, napi_callback_info info) {
+    apr_initialize();
+
+    apr_pool_t* pool;
+    apr_pool_create(&pool, NULL);
+
+    svn_client_ctx_t *ctx;
+    svn_client_create_context2(&ctx, NULL, pool);
+
+    svn_opt_revision_t rev;
+    memset(&rev, 0, sizeof(rev));
+
+    const char* path;
+
+    svn_dirent_get_absolute(&path, "C:\\tima\\poshsvn-trunk", pool);
+    path = svn_dirent_internal_style(path, pool);
+
+    svn_error_t* r = svn_client_info4(
+        path,
+        &rev,
+        &rev,
+        svn_depth_empty,
+        FALSE,
+        FALSE,
+        FALSE,
+        NULL,
+        NULL,
+        NULL,
+        ctx,
+        pool);
+
+    apr_pool_destroy(pool);
+
     napi_status status;
     napi_value world;
     status = napi_create_string_utf8(env, "world", 5, &world);
