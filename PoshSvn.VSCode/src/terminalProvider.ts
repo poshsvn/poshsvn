fbba6bf0ea3cfe8c8bbb0608b9e6f6ddb6f84192
Timofei Zhakov <tima@chemodax.net>
2024-05-05 13:29:11 +0000

    Invoke shell integration script.
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/trunk@1579 0083e6b4-6090-234a-836e-88fcc467669c

--- PoshSvn.VSCode/src/terminalProvider.ts
+++ PoshSvn.VSCode/src/terminalProvider.ts
@@ -2,6 +2,18 @@ import * as vscode from 'vscode';
 
 import { findPowerShell } from "./findPowerShell";
 import { helpMessage } from "./texts";
+import path from 'path';
+
+const shellIntegrationScript = path.join(
+    vscode.env.appRoot,
+    "out",
+    "vs",
+    "workbench",
+    "contrib",
+    "terminal",
+    "browser",
+    "media",
+    "shellIntegration.ps1");
 
 export const terminalOptions: vscode.TerminalOptions = {
     name: "PoshSvn terminal",
@@ -10,7 +22,9 @@ export const terminalOptions: vscode.TerminalOptions = {
     env: {
         "PSModulePath": __dirname
     },
-    shellArgs: `-NoExit -NoLogo -ExecutionPolicy RemoteSigned`,
+    // It should work, but it doesn't. I don't know why.
+    // TODO: fix it
+    shellArgs: `-NoExit -NoLogo -ExecutionPolicy RemoteSigned -Command "& '${shellIntegrationScript}'"`,
     // TODO: fill other parameters
 }
 
