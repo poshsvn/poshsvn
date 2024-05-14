fe1697e207c9f4024f79a13e10d906b2d3607cee
Timofei Zhakov <tima@chemodax.net>
2024-05-14 17:37:10 +0000

    Add tests on SvnRevisionChange.
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/trunk@1708 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ PoshSvn.Common.Tests/SvnRevisionChangeTests.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+using System;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace PoshSvn.Common.Tests
+{
+    public class SvnRevisionChangeTests
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            ClassicAssert.AreEqual(new SvnRevisionChange("123"), new SvnRevisionRange("122:123"));
+        }
+
+        [Test]
+        public void StartsWithRTest()
+        {
+            ClassicAssert.AreEqual(new SvnRevisionChange("r123"), new SvnRevisionRange("122:123"));
+        }
+
+        [Test]
+        public void NegativeTest()
+        {
+            ClassicAssert.AreEqual(new SvnRevisionChange("-123"), new SvnRevisionRange("123:122"));
+        }
+
+        [Test]
+        public void NegativeAndStartsWithRTest()
+        {
+            ClassicAssert.AreEqual(new SvnRevisionChange("-r123"), new SvnRevisionRange("123:122"));
+        }
+
+        [Test]
+        public void BadNegativeTest()
+        {
+            Assert.Throws<ArgumentException>(() => new SvnRevisionChange("r-123"));
+        }
+
+        [Test]
+        public void BadNonNumericTestHead()
+        {
+            Assert.Throws<ArgumentException>(() => new SvnRevisionChange("HEAD"));
+        }
+
+        [Test]
+        public void BadNonNumericTestQQQQQQQQQ()
+        {
+            Assert.Throws<ArgumentException>(() => new SvnRevisionChange("qqq"));
+        }
+    }
+}
