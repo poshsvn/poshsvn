73af07c3b7b8489187fcd00854d82e4faf7d698b
Timofei Zhakov <tima@chemodax.net>
2024-05-14 17:43:43 +0000

    SvnRevisionChangeTests: Use constructor from end and start revision in expectations instead of parsing it all the time.
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/trunk@1709 0083e6b4-6090-234a-836e-88fcc467669c

--- PoshSvn.Common.Tests/SvnRevisionChangeTests.cs
+++ PoshSvn.Common.Tests/SvnRevisionChangeTests.cs
@@ -11,25 +11,25 @@ namespace PoshSvn.Common.Tests
         [Test]
         public void SimpleTest()
         {
-            ClassicAssert.AreEqual(new SvnRevisionChange("123"), new SvnRevisionRange("122:123"));
+            ClassicAssert.AreEqual(new SvnRevisionChange("123"), new SvnRevisionRange(122, 123));
         }
 
         [Test]
         public void StartsWithRTest()
         {
-            ClassicAssert.AreEqual(new SvnRevisionChange("r123"), new SvnRevisionRange("122:123"));
+            ClassicAssert.AreEqual(new SvnRevisionChange("r123"), new SvnRevisionRange(122, 123));
         }
 
         [Test]
         public void NegativeTest()
         {
-            ClassicAssert.AreEqual(new SvnRevisionChange("-123"), new SvnRevisionRange("123:122"));
+            ClassicAssert.AreEqual(new SvnRevisionChange("-123"), new SvnRevisionRange(123, 122));
         }
 
         [Test]
         public void NegativeAndStartsWithRTest()
         {
-            ClassicAssert.AreEqual(new SvnRevisionChange("-r123"), new SvnRevisionRange("123:122"));
+            ClassicAssert.AreEqual(new SvnRevisionChange("-r123"), new SvnRevisionRange(123, 122));
         }
 
         [Test]
