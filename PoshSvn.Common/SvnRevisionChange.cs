adfcef11db211ca1e432dbfb2895c53e0eb66c78
Timofei Zhakov <tima@chemodax.net>
2024-05-15 12:39:19 +0000

    Follow-up to r1708: Add the SvnRevisionChange class. Forget commit ;)
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/trunk@1716 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ PoshSvn.Common/SvnRevisionChange.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+using System;
+
+namespace PoshSvn
+{
+    public class SvnRevisionChange : SvnRevisionRangeBase
+    {
+        public SvnRevisionChange(string str)
+        {
+            int i = 0;
+
+            // Check for a leading minus to allow "-c -r42".
+            // The is_negative flag is used to handle "-c -42" and "-c -r42".
+            // The "-c r-42" case is handled by strtol() returning a
+            // negative number.
+            bool isNegative = str[i] == '-';
+            if (isNegative)
+            {
+                i++;
+            }
+
+            // Allow any number of 'r's to prefix a revision number.
+            while (i < str.Length && (str[i] == 'r' || str[i] == ' '))
+            {
+                i++;
+            }
+
+            if (long.TryParse(str.Substring(i), out long revisionNumber))
+            {
+                if (revisionNumber < 0)
+                {
+                    throw new ArgumentException("Invalid revision: The revision number cannot be negative here.");
+                }
+
+                if (isNegative)
+                {
+                    StartRevision = new SvnRevision(revisionNumber);
+                    EndRevision = new SvnRevision(revisionNumber - 1);
+                }
+                else
+                {
+                    StartRevision = new SvnRevision(revisionNumber - 1);
+                    EndRevision = new SvnRevision(revisionNumber);
+                }
+            }
+            else
+            {
+                throw new ArgumentException($"Non-numeric change argument ({str}) given to -c");
+            }
+        }
+    }
+}
