893d6be07a12df5a8c6f58bb2c570d07f923df8a
Timofei Zhakov <tima@chemodax.net>
2024-12-20 12:41:02 +0000

    On the '1.1.x' branch: backport r3385,r3387 with some library updates and build improvements.
    git-svn-id: file:///c:/Repositories/rinrab/poshsvn/branches/1.1.x@3390 0083e6b4-6090-234a-836e-88fcc467669c

--- vcpkg/ports/serf/portfile.cmake
+++ vcpkg/ports/serf/portfile.cmake
@@ -1,5 +1,5 @@
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://www.apache.org/dist/serf/serf-1.3.10.zip"
+    URLS "https://archive.apache.org/dist/serf/serf-1.3.10.zip"
     FILENAME "serf-1.3.10.zip"
     SHA512 82e1c7342b0fa102c0e853989da0f6b590584e5a1d7737f891edd1d49b2a3ec271fd71f2642813455f73230c57230aebdc3a83808335dd53c5ce9fdab8506e2f
 )
@@ -7,27 +7,22 @@ vcpkg_download_distfile(ARCHIVE
 vcpkg_extract_source_archive(
     SOURCE_PATH
     ARCHIVE "${ARCHIVE}"
-    PATCHES
-        "serf-msbuild.patch"
 )
 
-vcpkg_install_msbuild(
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/build" DESTINATION "${SOURCE_PATH}")
+
+vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    PROJECT_SUBPATH "serf.sln"
-    TARGET "serf"
-    PLATFORM ${VCPKG_TARGET_ARCHITECTURE}
     OPTIONS
-        /p:CURRENT_INSTALLED_DIR=${CURRENT_INSTALLED_DIR}
-        /p:VCPKG_CRT_LINKAGE=${VCPKG_CRT_LINKAGE}
+        -DAPR_ROOT=${CURRENT_INSTALLED_DIR}
+        -DSKIP_SHARED=ON
+        -DSKIP_TESTS=ON
 )
+
+vcpkg_cmake_install()
+vcpkg_copy_pdbs()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
-# Header files
-file(GLOB_RECURSE INCLUDES "${SOURCE_PATH}/*.h")
-file(COPY ${INCLUDES} DESTINATION "${CURRENT_PACKAGES_DIR}/include/serf-1")
-
 configure_file("${SOURCE_PATH}/build/serf.pc.in" "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/serf.pc" @ONLY)
-
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/serf-config.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
